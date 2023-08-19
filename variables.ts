f266115f42f9ef0236d7af25ae62d42e1f16ee2a
Hikaru Nguyen <quang.nm2201@gmail.com>
2023-08-20 00:10:17 +0900

    update 2023-08-19

--- variables.ts
+++ variables.ts
@@ -30,3 +30,30 @@ let point: { x: number; y: number } = {
 const logNumber = (i: number) => {
     console.log(i);
 };
+
+//When to use annotations；
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number; y: number} = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare variable on one line
+// and initilizate it later
+let words = ['red', 'green', 'blue'];
+let foundWord = false;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
