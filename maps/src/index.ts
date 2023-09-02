c56d9bd372d8132d5e1f6be4d1745b564669af79
Hikaru Nguyen <quang.nm2201@gmail.com>
2023-09-03 00:28:34 +0900

    update using googlemap API

---
+++ maps/src/index.ts
@@ -0,0 +1,17 @@
+// import { User } from "./Users";
+// import { Company } from "./Company";
+
+
+// const user = new User();
+// const company = new Company();
+
+// console.log(user);
+// console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
