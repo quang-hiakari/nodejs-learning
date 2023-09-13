237c9c9806e7a8991e0819b842c0f96c70353a4b
Quang Nguyen <quang.nm2201@gmail.com>
2023-09-13 17:40:28 +0900

    update

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,21 +1,19 @@
 class Sorter {
-    constructor(public collection: number[]) {};
+    constructor(public collection: NumbersCollection) {};
 
     sort(): void {
         const { length } = this.collection;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                if (this.collection.compare()) {
+                    this.collection.swap();
                 }
             }
         }
     }
 }
 
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(new );
 sorter.sort();
 console.log(sorter.collection);
