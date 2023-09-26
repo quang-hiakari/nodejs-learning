7e98ebb9f02f25a938efd57d74beffd4712967b8
Quang Nguyen <quang.nm2201@gmail.com>
2023-09-26 17:45:03 +0900

    update AppProps

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -5,13 +5,33 @@ interface AppProps {
     color?: string;
 }
 
+// const App = (props: AppProps): JSX.Element => {
+//     return <div>{props.color}</div>
+// };
+
 class App extends React.Component<AppProps> {
+    state = { counter: 0 };
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1});
+    };
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1});
+    };
+
     render() {
-        return <div>{this.props.color}</div>
+        return (
+            <div>
+                <button onClick={this.onIncrement} >Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        );
     }
 }
 
 ReactDOM.render(
-    <App color="red" />,
+    <App color="red"/>,
     document.querySelector("#root")
 );
